9191dc8385ae78ee2fed671fdfb2fbb15ebd6eb4
Sean A. Hanley <yadyn@yahoo.com>
2012-07-09 13:12:55 -0500

    Corrected assembly version and gitignore
    Changed the assembly version to match as published. Also, added NuGet
    files to the git ignore list.

--- TaskDialog/Properties/AssemblyInfo.cs
+++ TaskDialog/Properties/AssemblyInfo.cs
@@ -3,16 +3,16 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-[assembly: AssemblyTitle("TaskDialog")]
-[assembly: AssemblyDescription("A TaskDialog wrapper and emulator for WPF.")]
+[assembly: AssemblyTitle("WPF Task Dialog")]
+[assembly: AssemblyDescription("A TaskDialog wrapper class with fallback emulator (for XP and earlier).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("TaskDialog")]
-[assembly: AssemblyCopyright("© Sean A. Hanley 2010")]
+[assembly: AssemblyCopyright("© Sean A. Hanley 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb889d00-23dd-4fdf-967d-10cbbbfb086d")]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
